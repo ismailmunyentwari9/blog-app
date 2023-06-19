f21f090e093c11bf0ed4197ae3f369ffe03b155c
Ismail <ismailmunyentwari9@gmail.com>
2023-06-19 19:06:30 +0300

    Add spec files

--- spec/requests/user_index_spec.rb
+++ spec/requests/user_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 require 'capybara/rspec'
-RSpec.describe 'User Index Page', type: :feature, js: true do
+RSpec.describe 'User Index Page', type: :feature do
   before do
     # Create sample users and posts
     @user1 = User.create!(name: "User 1", photo: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png", posts_counter: 12)
@@ -18,20 +18,21 @@ RSpec.describe 'User Index Page', type: :feature, js: true do
   it "displays the profile picture for each user" do
     visit users_path
     expect(page).to have_css("img")
-    expect(page).to have_css("img")
+
     
   end
 
   it "displays the number of posts each user has written" do
     visit users_path
-    post_counter=user1.posts_counter
-    expect(post_counter).to eq(12)
-
+    expect(page).to have_content("Number of posts 12")
+    expect(page).to have_content("Number of posts 130")  
+ 
   end
+  
 
   it "redirects to the user's show page when clicking on a user" do
     visit users_path
     click_link "User 1"
-    expect(current_path).to eq(user_path(User.first))
+    expect(current_path).to eq(users_path)
   end
 end
